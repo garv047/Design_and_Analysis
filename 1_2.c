803747ffb66a3ebb76d1bf677cceea8855cc97c0
Garv Bhatia <bhatiagarv07@gmail.com>
2026-08-03 19:42:51 +0530

    Create 1_2.c

---
+++ 1_2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int key;
+    scanf("%d", &key);
+
+    int low = 0, high = n - 1;
+    int result = -1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key) {
+            result = mid;
+            break;
+        } else if (arr[mid] < key) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    printf("%d\n", result);
+
+    return 0;
+}
